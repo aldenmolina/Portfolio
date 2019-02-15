297388029a79b72a36eb48fac712add57c97f331
Alden <aj.molina092@gmail.com>
2019-02-15 14:55:31 -0500

    Redesigned. Requirements met.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// Sticky menu background
+window.addEventListener('scroll', function () {
+  if (window.scrollY > 50) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+
+// Smooth Scrolling
+$('#navbar a, .button').on('click', function (event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+// Add animation on scroll
+$(document).ready(function () {
+  var scroll_pos = 0;
+  $(document).scroll(function () {
+    scroll_pos = $(this).scrollTop();
+    if (scroll_pos > 1350) {
+      $("nav h1").css('color', 'white');
+      $("nav a").css('color', 'white');
+    }
+
+  }
+  )
+}
+)
